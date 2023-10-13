airfoil lift calculation corrected

main.py
@@ -33,9 +33,9 @@ for am in airfoils:
     for pi in am.panels:
         for an in airfoils:
             for pj in an.panels:
-                pi.velIndCalc(pj.qPoint)
+                pi.unitVelIndCalc(pj.qPoint)
 
-#print(airfoils[0].panels[0].velInd)
+#print(airfoils[0].panels[0].unitVelInd)
 
 #sys.exit()
 
@@ -62,7 +62,7 @@ i = 0
 for a in airfoils:
     for pi in a.panels:
         for j in range(N):
-            A[i,j] = np.dot(pi.velInd[j], pi.n)
+            A[i,j] = np.dot(pi.unitVelInd[j], pi.n)
 
         i = i + 1
 
@@ -79,7 +79,14 @@ for a in airfoils:
         p.dCpCalc(vInf)
         i = i + 1
 
-    a.liftCalc(vInf)
+for aa in airfoils:
+    for p in aa.panels:
+        for ab in airfoils:
+            p.totalIndVelCalc(ab.panels)
+
+        p.liftCalc(vectorInf)
+
+    aa.liftCalc(vInf)
 
 writeResults(airfoils)
 

utils.py
@@ -9,9 +9,11 @@ class Node:
 # panel class definition
 class Panel:
     def __init__(self, nodes, name):
-        self.velInd = []
+        self.unitVelInd = []
+        #self.totalVelInd = []
         self.nodes = nodes
         self.id = name
+        self.lift = 0.0
 
         # tangent unit vector and panel length
         tVec = self.nodes[1].coord - self.nodes[0].coord
@@ -33,18 +35,37 @@ class Panel:
         # midpoint coordinates
         self.mPoint = self.nodes[0].coord + 0.5*tVec
 
-    def velIndCalc(self, quarterPoint):
+    # calculation of induced velocity produced by unit vortex 
+    def unitVelIndCalc(self, quarterPoint):
         vector =  self.cPoint - quarterPoint 
         r = np.linalg.norm(vector)
         u = (0.5/(np.pi*r**2))*vector[1]
         v = - (0.5/(np.pi*r**2))*vector[0]
-        self.velInd.append(np.array([u, v, 0.0]))
+        self.unitVelInd.append(np.array([u, v, 0.0]))
 
     def setGamma(self, gamma):
         self.gamma = gamma
 
+    # delta Cp calculation across panel
     def dCpCalc(self, velInf):
         self.dCp = 2*self.gamma/(velInf*self.len)
+
+    # total velocity induced by all vortices
+    def totalIndVelCalc(self, panels):
+        self.totalVelInd = np.array([0.0, 0.0, 0.0])
+        for p in panels:
+            distVec =  self.qPoint - p.qPoint 
+            r = np.linalg.norm(distVec)
+
+            if r != 0.0:
+                u = p.gamma*(0.5/(np.pi*r**2))*distVec[1]
+                v = - p.gamma*(0.5/(np.pi*r**2))*distVec[0]
+                self.totalVelInd = self.totalVelInd + np.array([u, v, 0.0])
+
+    # panel lift calculation (generalized Kutta-Joukowski)
+    def liftCalc(self, velInf):
+        vInf = np.linalg.norm(velInf)
+        self.lift = 1.2*vInf*self.gamma*(1 + np.dot(velInf, self.totalVelInd)/vInf**2)
 
 # airfoil class definition
 class Airfoil:
@@ -74,10 +95,13 @@ class Airfoil:
     # aerodynamic characteristics: Gamma, L and Cl
     def liftCalc(self, vInf):
         self.Gamma = 0.0
+        self.L = 0.0
+
         for p in self.panels:
             self.Gamma = self.Gamma + p.gamma
+            self.L = self.L + p.lift
 
-        self.L = 1.2*vInf*self.Gamma
+        #self.L = 1.2*vInf*self.Gamma
         self.Cl = 2.0*self.L/(1.2*vInf**2*self.c)
         
 # write results to file and terminal
@@ -93,8 +117,8 @@ def writeResults(airfoils):
             print('Cl = {0:.4f}\n'.format(a.Cl))
             print(40*'=' + '\n')
             
-            out.write('Airfoil #{0:d} aerodynamic characteristics:'.format(list(airfoils).index(a)+1))
-            out.write('Gamma = {0:.4f} m2/s'.format(a.Gamma))
-            out.write('L = {0:.4f} N/m'.format(a.L))
+            out.write('Airfoil #{0:d} aerodynamic characteristics:\n'.format(list(airfoils).index(a)+1))
+            out.write('Gamma = {0:.4f} m2/s\n'.format(a.Gamma))
+            out.write('L = {0:.4f} N/m\n'.format(a.L))
             out.write('Cl = {0:.4f}\n'.format(a.Cl))
             out.write(40*'=' + '\n')